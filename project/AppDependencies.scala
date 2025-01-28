90c17f15bff183799a72d7cdb793e3095a9c1f5d
Geethu Elizebeth Mathew <32937184+geethumathew@users.noreply.github.com>
2025-01-28 11:23:42 +0000

    DL-15293:Rectifying bobby violation (#98)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ object AppDependencies {
 
   import play.sbt.PlayImport._
 
-  val boostrapVersion = "9.6.0"
+  val boostrapVersion = "9.7.0"
   val compile: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc" %% "bootstrap-backend-play-30" % boostrapVersion,
