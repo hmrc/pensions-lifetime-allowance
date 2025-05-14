9ed0ad251c62a8189c65af4e3141f7767a3b8e68
JoshMiernik <63236120+JoshMiernik@users.noreply.github.com>
2025-05-14 09:26:55 +0100

    DL-16242 removing unused code (#103)
    * DL-16242 removing unused code
    
    * DL-16242 removed test and scalafmt formatting

--- it/test/connectors/NPSConnectorSpec.scala
+++ it/test/connectors/NPSConnectorSpec.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package connectors
 
 /*
@@ -73,12 +89,6 @@ class NPSConnectorSpec extends PlaySpec with MockitoSugar {
       } catch {
         case ex: Throwable =>
       }
-    }
-  }
-
-  "The NPS Connector getApplyUrl method" when {
-    "return a  URL that contains the nino passed to it" in {
-      testNPSConnector.getApplyUrl(testNinoWithoutSuffix).contains(testNinoWithoutSuffix) shouldBe true
     }
   }
 
