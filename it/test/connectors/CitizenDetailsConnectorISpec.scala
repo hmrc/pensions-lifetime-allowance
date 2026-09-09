0f6367a600a9445b1d747d9ca465e01f80e3c6da
eloiseyaz <79723491+eloiseyaz@users.noreply.github.com>
2026-09-09 14:13:31 +0100

    get it tests running

--- it/test/connectors/CitizenDetailsConnectorISpec.scala
+++ it/test/connectors/CitizenDetailsConnectorISpec.scala
@@ -34,7 +34,7 @@ package connectors
 
 import com.github.tomakehurst.wiremock.client.WireMock.{aResponse, get, urlPathMatching}
 import org.scalatest.BeforeAndAfter
-import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
+import org.scalatest.matchers.should.Matchers.shouldBe
 import org.scalatestplus.mockito.MockitoSugar
 import play.api.http.Status._
 import play.api.inject.guice.GuiceableModule
